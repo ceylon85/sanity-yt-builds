47a14d67bddb7aa9be233387b32486530c26e105
ceylon85 <canleez123@gmail.com>
2022-04-21 20:21:27 +0900

    herader

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,21 +3,25 @@ import Link from "next/link"
 function Header() {
     return (
 
-        <header className="flex">
-            <div>
+        <header className="flex justify-between p-5 mx-auto max-w-7xl borders">
+            <div className="flex items-center space-x-5">
                 <Link href="/" >
-                    <img className="object-contain cursor-pointer w-44" src="https://links.papareact.com/yvf" alt="logo" />
+                    <img className="object-contain w-48 cursor-pointer" src="https://links.papareact.com/yvf" alt="logo" />
                 </Link>
-                <div>
-                    <h3></h3>
-                    <h3></h3>
-                    <h3></h3>
+                <div className="items-center hidden space-x-5 md:inline-flex">
+                    {/* Out Story */}
+                    <h3>About</h3>
+                    {/* Contact */}
+                    <h3>Membership</h3>
+                    {/* Follow */}
+                    <h3 className="px-4 py-1 text-white bg-green-600 rounded-full">Write</h3>
                 </div>
             </div>
-            <div className="">
-                <div>Sign In</div>
-                <div>Get started</div>
+            <div className="flex items-center space-x-5 text-green-600 ">
+                <h3 className="hidden md:inline-flex">Sign In</h3>
+                <h3 className="px-4 py-1 border border-green-600 rounded-full">Get started</h3>
             </div>
+
         </header>)
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,21 @@ import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-yellow-500" >
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div>
+        <div className='px-10 space-y-5'>
+          <h1 className='max-w-xl font-serif text-7xl'>Stay curious.</h1>
+          <h2>Discover stories, thinking, and <br />
+            expertise from writes on any topic</h2>
+            <h2 className='px-5 py-3 text-white bg-black rounded-full w-36'>Start reading</h2>
+          
+        </div>
+      </div>
     </div>
 
   )
