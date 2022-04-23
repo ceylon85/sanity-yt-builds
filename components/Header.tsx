2e0a5f29c5850206aa58ee6bd6a63ce02793c6e6
ceylon85 <canleez123@gmail.com>
2022-04-23 09:31:22 +0900

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link"
 
 function Header() {
     return (
-        // 헤더 고정 및 스크롤 내릴 때 색상변경
-        <header className="flex justify-between p-3 pb-2 mx-auto border-b border-solid pl-14 max-w-7xl border-b-black">
+        // 헤더 고정 및 스크롤 내릴 때 색상변경 해야한다
+        <header className="flex justify-between p-2 pb-2 mx-auto border-b border-solid pl-14 max-w-7xl border-b-black">
             <div className="flex items-center space-x-5 text-sm">
                 <Link href="/" >
                     <img className="object-contain w-48 cursor-pointer" src="https://links.papareact.com/yvf" alt="logo" />
@@ -17,9 +17,9 @@ function Header() {
                     <h3 className="px-4 py-3 border-black rounded-full">Write</h3>
                 </div>
             </div>
-            <div className="flex items-center py-2 space-x-5 text-sm">
+            <div className="flex items-center py-2 mr-6 space-x-5 text-sm">
                 <h3 className="hidden md:inline-flex">Sign In</h3>
-                <h3 className="px-4 py-3 font-light text-white bg-black rounded-full">Get started</h3>
+                <h3 className="px-4 py-3 text-white bg-black rounded-full">Get started</h3>
             </div>
 
         </header>)
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,14 @@ import { Player, Controls } from '@lottiefiles/react-lottie-player';
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-yellow-500 " >
+    <div className="mx-auto bg-yellow-500 max-w-7xl" >
       <Head>
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <div className='flex items-center justify-between border-b border-b-black md:py-4 lg:py-0'>
-        <div className='p-10 space-y-5 '>
+        <div className='space-y-4 p-9 '>
           <h1 className='max-w-xl font-serif text-7xl lg:text-8xl'>Stay curious.</h1>
           <h2 className='pb-8 text-2xl'>Discover stories, thinking, and <br />
             expertise from writes on any topic.</h2>
