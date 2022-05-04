595c47401d0b9ab139c7e7e541ec2a7352d4b5e3
ceylon85 <canleez123@gmail.com>
2022-05-04 17:19:58 +0900

    post

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -17,17 +17,38 @@ function Post({ post }: Props) {
             <article className='max-w-3xl p-5 mx-auto'>
                 <h1 className='mt-10 mb-3 text-3xl'>{post.title}</h1>
                 <h2 className='mb-2 text-xl font-light text-gray-500'>{post.description}</h2>
-                <div className='flex items-center rounded-full'>
-                    <img className='w-10 h-10 rounded-full' src={urlFor(post.author.image).url()!} alt="author" />
+                <div className='flex items-center space-x-2'>
+                    <img className='w-10 h-10 rounded-full'
+                        src={urlFor(post.author.image).url()!} alt="author" />
                     <p className='text-sm font-extralight'>
                         Blog post by {" "}
                         <span className='text-green-600'>{post.author.name}</span> - Published at {new Date(post._createdAt).toLocaleString()}
                     </p>
                 </div>
-                <div>
+                <div className='mt-10'>
                     <PortableText
                         dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
-                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!} />
+                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                        content={post.body}
+                        serializers={
+                            {
+                                h1: (props: any) => (
+                                    <h1 className='my-5 text-2xl font-bold'{...props} />
+                                ),
+                                h2: (props: any) => (
+                                    <h1 className='my-5 text-xl font-bold'{...props} />
+                                ),
+                                li: (children: any) => (
+                                    <li className='ml-4 list-disc'>{children}</li>
+                                ),
+                                link: ({ href, children }: any) => (
+                                    <a href={href} className="text-blue-500 hover:underline">
+                                        {children}
+                                    </a>
+                                ),
+                            }
+                        }
+                    />
                 </div>
             </article>
         </main>
--- sanityytbuilds/index.d.ts
+++
@@ -1,6 +0,0 @@
-import PortableText from "react-portable-text";
-
-export default PortableText;
-export function blockContentToPlainText(bloclk?:[any]):string;
-
-@param {object} 
