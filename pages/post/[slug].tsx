35f0bf84dca244c58b393543cc25351a602c86e6
ceylon85 <canleez123@gmail.com>
2022-05-09 19:40:46 +0900

    comment

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,6 +4,7 @@ import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typing'
 import PortableText from 'react-portable-text'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import { useState } from 'react'
 
 interface IFormInput {
     _id: string;
@@ -17,6 +18,9 @@ interface Props {
 
 function Post({ post }: Props) {
     //console.log(post); post data 확인됨
+
+    const [submitted, setSubmitted] = useState(false)
+
     const {
         register,
         handleSubmit,
@@ -30,8 +34,10 @@ function Post({ post }: Props) {
             body: JSON.stringify(data),
         }).then(() => {
             console.log(data);
+            setSubmitted(true);
         }).catch((error) => {
             console.log(error)
+            setSubmitted(false)
         })
     }
 
@@ -79,50 +85,59 @@ function Post({ post }: Props) {
             </article>
 
             <hr className='max-w-lg mx-auto my-5 border border-yellow-500' />
-
-            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col max-w-2xl p-5 mx-auto mb-10'>
-                <h3 className='text-sm text-yellow-500'>Enjoyed the this Article?</h3>
-                <h4 className='text-3xl font-bold'>Leave a comment below!</h4>
-                <hr className='py-3 mt-2' />
-
-                <input
-                    {...register("_id")}
-                    type="hidden"
-                    name='_id'
-                    value={post._id} />
-
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Name</span>
-                    <input {...register('name', { required: true })}
-                        className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 focus:ring' placeholder='John Doe' type="text" />
-                </label>
-
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Email</span>
-                    <input {...register('email', { required: true })}
-                        className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 focus:ring' placeholder='abc@email.com' type="email" />
-                </label>
-
-                <label className='block mb-5'>
-                    <span className='text-gray-700'>Comment</span>
-                    <textarea {...register('comment', { required: true })}
-                        className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 form-textarea focus:ring' placeholder='내용을 써주세욥' rows={8} />
-                </label>
-
-                <div className='flex flex-col p-5'>
-                    {errors.name && (
-                        <span className='text-red-600'>- The Name Field is required</span>
-                    )}
-                    {errors.email && (
-                        <span className='text-red-600'>- The email Field is required</span>
-                    )}
-                    {errors.comment && (
-                        <span className='text-red-600'>- The comment Field is required</span>
-                    )}
+            {submitted ? (
+                <div className='flex flex-col max-w-2xl p-10 mx-auto my-10 text-white bg-yellow-500'>
+                    <h3 className='text-3xl font-bold'>의견을 보내주셔서 감사합니다!! @.@</h3>
+                    <p>
+                        승인되면 아래에 표시됩니다!
+                    </p>
                 </div>
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col max-w-2xl p-5 mx-auto mb-10'>
+                    <h3 className='text-sm text-yellow-500'>Enjoyed the this Article?</h3>
+                    <h4 className='text-3xl font-bold'>Leave a comment below!</h4>
+                    <hr className='py-3 mt-2' />
 
-                <input className='px-4 py-2 font-bold text-white bg-yellow-500 rounded shadow cursor-pointer hover:bg-yellow-400 focus:outline-none' type="submit" />
-            </form>
+                    <input
+                        {...register("_id")}
+                        type="hidden"
+                        name='_id'
+                        value={post._id} />
+
+                    <label className='block mb-5'>
+                        <span className='text-gray-700'>Name</span>
+                        <input {...register('name', { required: true })}
+                            className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 focus:ring' placeholder='John Doe' type="text" />
+                    </label>
+
+                    <label className='block mb-5'>
+                        <span className='text-gray-700'>Email</span>
+                        <input {...register('email', { required: true })}
+                            className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 focus:ring' placeholder='abc@email.com' type="email" />
+                    </label>
+
+                    <label className='block mb-5'>
+                        <span className='text-gray-700'>Comment</span>
+                        <textarea {...register('comment', { required: true })}
+                            className='block w-full px-3 py-2 mt-1 border rounded shadow outline-none ring-yellow-500 form-textarea focus:ring' placeholder='내용을 써주세욥' rows={8} />
+                    </label>
+
+                    <div className='flex flex-col p-5'>
+                        {errors.name && (
+                            <span className='text-red-600'>- The Name Field is required</span>
+                        )}
+                        {errors.email && (
+                            <span className='text-red-600'>- The email Field is required</span>
+                        )}
+                        {errors.comment && (
+                            <span className='text-red-600'>- The comment Field is required</span>
+                        )}
+                    </div>
+
+                    <input className='px-4 py-2 font-bold text-white bg-yellow-500 rounded shadow cursor-pointer hover:bg-yellow-400 focus:outline-none' type="submit" />
+                </form>
+            )}
+
 
         </main>
     )
